1d3a5dbc15aafa28201a1358015c22ac86eb6549
Dv <dv@Dvs-MacBook-Pro.local>
2020-12-14 22:51:16 +0530

    counter

--- src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -1,7 +1,24 @@
 import React, { useState } from 'react';
 
+
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+  const [people, setPeople] = useState({
+  name:"Peter",
+  age:26,
+  message:"Hi it's Peter here."
+  });
+const change = () => {
+  setPeople({...people, message:"Hello"});
+}
+
+  return (
+    <>
+    <h2>{people.name}</h2>
+    <h2>{people.age}</h2>
+    <h2>{people.message}</h2>
+    <button className="btn" onClick={change}>Change Message</button>
+    </>
+  )
 };
 
 export default UseStateObject;
--- src/tutorial/1-useState/setup/5-useState-counter.js
+++ src/tutorial/1-useState/setup/5-useState-counter.js
@@ -1,7 +1,34 @@
 import React, { useState } from 'react';
 
 const UseStateCounter = () => {
-  return <h2>useState counter example</h2>;
+  const [value, setValue] = useState(0);
+const reset = () => {
+  setValue(0);
+}
+
+const complex = () => {
+  setTimeout( () =>{
+    setValue((prevState) => {
+      return prevState + 1;
+    });
+  }, 3000);
+}
+
+  return (
+    <>
+    <section style={{margin: '4rem 0'}}>
+<h2>Regular Counter</h2>
+<h1>{value}</h1>
+<button className="btn" onClick={() => setValue(value - 1)}>Decrease</button>
+<button className="btn" onClick={reset}>Reset</button>
+<button className="btn" onClick={() => setValue(value + 1)}>Increase</button>
+    </section>
+    <section style={{margin:'5rem 0'}}>
+      <h1>{value}</h1>
+      <button className="btn" onClick={complex}>Complex Increse</button>
+    </section>
+    </>
+  );
 };
 
 export default UseStateCounter;
