e7a55d39054bb4e5b325b9fc17aa5e1c058f68b3
Dv <dv@Dvs-MacBook-Pro.local>
2020-12-31 21:06:17 +0530

    useRef

--- src/tutorial/5-useRef/setup/1-useRef-basics.js
+++ src/tutorial/5-useRef/setup/1-useRef-basics.js
@@ -1,11 +1,31 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef } from "react";
 
 // preserves value
 // DOES NOT trigger re-render
 // target DOM nodes/elements
 
 const UseRefBasics = () => {
-  return <h2>useRef</h2>;
+  const [name, setName] = useState("");
+
+  const refContainer = useRef(null);
+  const handleChange = (e) => {
+    e.preventDefault();
+    console.log(refContainer.current.value);
+  };
+
+  useEffect(() => {
+    console.log(refContainer.current);
+    refContainer.current.focus();
+  });
+
+  return (
+    <form className="form" onSubmit={handleChange}>
+      <input type="text" ref={refContainer} />
+      <button type="submit" className="btn">
+        submit
+      </button>
+    </form>
+  );
 };
 
 export default UseRefBasics;
