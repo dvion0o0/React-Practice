aa41d95c01fac1d1e621f2ae0df55e23c45da2aa
Dv <dv@Dvs-MacBook-Pro.local>
2021-01-07 00:03:53 +0530

    prop-types

--- src/tutorial/10-prop-types/setup/Product.js
+++ src/tutorial/10-prop-types/setup/Product.js
@@ -1,7 +1,27 @@
-import React from 'react';
+import React from "react";
+import PropTypes from "prop-types";
+import defaultImage from "../../../assets/default-image.jpeg";
+const Product = ({ name, image, price }) => {
+  const url = image && image.url;
+  return (
+    <article className="product">
+      <img src={url || defaultImage} alt={name || "Default Name"} />
+      <h4>{name || "Default Name"}</h4>
+      <p>${price || 3.99}</p>
+    </article>
+  );
+};
 
-const Product = () => {
-  return <article className='product'>single product</article>;
+Product.prototype = {
+  image: PropTypes.object.isRequired,
+  name: PropTypes.string.isRequired,
+  price: PropTypes.number.isRequired,
+};
+
+Product.defaultProps = {
+  image: defaultImage,
+  name: "Default Name",
+  price: 3.99,
 };
 
 export default Product;
