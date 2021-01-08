e1d4a13094691675f4efc2f944e66103014fa8e0
Dv <dv@Dvs-MacBook-Pro.local>
2021-01-08 19:00:21 +0530

    react router

--- src/tutorial/11-react-router/setup/Person.js
+++ src/tutorial/11-react-router/setup/Person.js
@@ -1,10 +1,21 @@
-import React, { useState, useEffect } from 'react';
-import { data } from '../../../data';
-import { Link, useParams } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import { data } from "../../../data";
+import { Link, useParams } from "react-router-dom";
 const Person = () => {
+  const { id } = useParams();
+  const [name, setName] = useState("Default Name");
+
+  useEffect(() => {
+    const newPerson = data.find((item) => item.id === parseInt(id));
+    setName(newPerson.name);
+  }, []);
+
   return (
     <div>
-      <h2>person</h2>
+      <h1>{name}</h1>
+      <Link to="/people" className="btn">
+        Back to People
+      </Link>
     </div>
   );
 };
